89e4b735874a9543e2c78e750ff9f69fac975eb5
truvor <siakimenko@proton.me>
2026-02-20 16:34:58 -0500

    Output Bitcoin price from Coinbase over 10 seconds

---
+++ main.js
@@ -0,0 +1,27 @@
+const ws = new WebSocket("wss://ws-feed.exchange.coinbase.com");
+ws.binaryType = "arraybuffer";
+
+ws.onopen = () => {
+    console.log("Connection opened. Subscribing to BTC-USD ticker.");
+    ws.send(JSON.stringify({
+        "type": "subscribe",
+        "channels": [{ "name": "ticker", "product_ids": ["BTC-USD"] }]
+    }));
+};
+
+ws.onmessage = (event) => {
+    const price = JSON.parse(event.data).price;
+    if (price !== undefined) console.log(price)
+};
+
+ws.onclose = (event) => {
+    console.log("Connection closed", event.code, event.reason, event.wasClean);
+};
+
+ws.onerror = () => {
+    console.log("Connection closed due to error");
+};
+
+setTimeout(() => {
+    ws.close();
+}, 10000);
